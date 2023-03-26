f382bbed45e106d60e41635674793d64750b790f
Logan Hankla <chankla@smith.edu>
2023-03-26 19:26:30 -0400

    House trying by myself

--- House.java
+++ House.java
@@ -1,19 +1,46 @@
-/* This is a stub for the House class */
+/**
+ * Filename: House.java
+ * Description: The House class is a subclass of the superclass, Building. It stores information about the house & residents.
+ * @author Logan Hankla
+ * Date: March 2023
+ */
 
 import java.util.ArrayList;
 
 public class House extends Building {
 // in class added "extends Building"
   
-  private ArrayList<String> residents; //in class
-  private boolean hasDiningRoom; //in class
+  private ArrayList<String> residents; 
+  private boolean hasDiningRoom; 
 
 
   public House(String name, String address, int nFloors, boolean diningRoom) {
     //System.out.println("You have built a house: 🏠");
-    super(name, address, nFloors); //in class
-    this.residents = new ArrayList<String>(); //class
-    this.hasDiningRoom = diningRoom; //class
+    super(name, address, nFloors); 
+    this.residents = new ArrayList<String>(); 
+    this.hasDiningRoom = diningRoom; 
+    this.nResidents = residents.size(); //check
+  }
+
+  public void moveIn(String name){ //check
+    this.residents.add(name);
+  }
+
+  public String moveOut(String name){ //check
+    if(this.residents.contains(name)){
+      this.residents.remove(name);
+      return name;
+    } else{
+      throw new RuntimeException("This person does not live in this house.");
+    }
+  }
+
+  public boolean isResident(String person){ //check
+    if(this.residents.contains(person)){
+      return true;
+    } else{
+      return false;
+    }
   }
 
   public String toString() {
@@ -31,7 +58,7 @@ public class House extends Building {
   }
   public static void main(String[] args) {
     //new House();
-    House wilson = new House("Wilson House", "16 Kensington Ave", 4);
+    House wilson = new House("Wilson House", "16 Kensington Ave", 4, true);
   }
 
 }
